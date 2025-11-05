3302cf0415ac2b35dac16459cc843a730225546f
faik dursun <250541124@firat.edu.tr>
2025-11-05 14:56:22 +0300

    Implement student information system in OgrenciBilgi.java

--- OgrenciBilgi.java
+++ OgrenciBilgi.java
@@ -1,7 +1,7 @@
 /*
- * Ad Soyad: [ADINIZI BURAYA YAZIN]
- * Ogrenci No: [OGRENCI NUMARANIZI BURAYA YAZIN]
- * Tarih: [TARIHI BURAYA YAZIN]
+ * Ad Soyad: [faik dursun]
+ * Ogrenci No: [250541124]
+ * Tarih: [05.11.2025]
  * Aciklama: Gorev 1 - Ogrenci Bilgi Sistemi
  * 
  * Bu program kullanicidan ogrenci bilgilerini alir ve
@@ -11,50 +11,33 @@
 
 import java.util.Scanner;
 
-public class OgrenciBilgi {
+public class ogrencibilgi {
     public static void main(String[] args) {
-        // Scanner objesi olusturun
-        
-        
-        // Degisken tanimlamalari
-        // String ad, soyad;
-        // int ogrenciNo, yas;
-        // double gpa;
-        
-        
-        // Kullanicidan bilgileri alin
-        System.out.println("=== OGRENCI BILGI SISTEMI ===");
-        System.out.println();
-        
-        // Ad
-        
-        
-        // Soyad
-        
-        
-        // Ogrenci No
-        
-        
-        // Yas
-        
-        
-        // GPA
-        
-        
-        // Bilgileri ekrana yazdirin
-        System.out.println("\n=== OGRENCI BILGI SISTEMI ===");
-        
-        // COZUMUNUZU BURAYA YAZIN
-        
-        
-        
-        
-        
-        
+        Scanner input = new Scanner(System.in);
 
-        
-        
-        // Scanner'i kapatin (önemli pratik)
-        
+        System.out.print(" Adınızı giriniz: ");
+        String ad = input.nextLine();
+
+        System.out.print(" soyadınız giriniz: ");
+        String soyad = input.nextLine();
+
+        System.out.print(" yasınızı giriniz: ");
+        int yas = input.nextInt();
+
+        System.out.print(" Ögrenci numarasını giriniz: ");
+        int ogrenciNo = input.nextInt();
+
+        System.out.print(" GPA (0,00 - 4,00): ");
+        double ogrenciGPA = input.nextDouble();
+
+        System.out.println("\n-------Ögrenci Bilgileri------");
+        System.out.println("AD: "+ad);
+        System.out.println("SOYAD: "+soyad);
+        System.out.println("yas:"+yas);
+        System.out.println("GPA: "+ogrenciGPA);
+        System.out.println("OGRENCİNO: "+ogrenciNo);
+
+        input.close();
+
     }
 }
